fed6b1c296f71c279fa792755c9445ef8768563d
Layla Stein <layla.stein@gmail.com>
2024-05-30 03:14:56 +0300

    sql changed to postgres syntax

--- DB1.sql
+++ DB1.sql
@@ -1,64 +1,83 @@
-create table wages(
-                                id number(10) primary key,
-                                jobId number(10) references jobs
-                                types varchar(50),
-                                wage decimal(10, 2)
-                  );
-                  
-create table procurements(
-                                id number(10) primary key,
-                                types varchar(50)
-                         );
+-- SCHEMA: billing
 
-create table subscriptions(
-                                id number(10) primary key,
-                                types varchar(50),
-                                cost decimal(10, 2),
-                                isRenewable number(1)
-                          );
+CREATE SCHEMA IF NOT EXISTS billing
+    AUTHORIZATION postgres;
 
-create table penalties(
-                                id number(10) primary key,
-                                types varchar(50),
-                                fee decimal(10, 2)
-                      );
+CREATE TABLE IF NOT EXISTS billing.books
+(
+  id_ numeric(10,0) primary key
+);
 
-create table insurance(
-                                id number(10) primary key,
-                                providerId number(10) references providers,
-                                plan varchar(50),
-                                cost decimal(10, 2)
-                      );
+CREATE TABLE IF NOT EXISTS billing.providers
+(
+  id_ numeric(10,0) primary key
+);
 
-create table funding(
-                                id number(10) primary key,
-                                types varchar(50)
-                    );
+CREATE TABLE IF NOT EXISTS billing.employees
+(
+  id_ numeric(9,0) primary key
+);
 
-create table employees(
-                               id number(9) primary key
-                      );
-                  
-create table books(
-                               id number(10) primary key
-                  );
+CREATE TABLE IF NOT EXISTS billing.members
+(
+  id_ numeric(9,0) primary key
+);
 
-create table members(
-                                id number(9) primary key
-                    );
+CREATE TABLE IF NOT EXISTS billing."insurableEntities"
+(
+  id_ numeric(10,0) primary key
+);
 
-create table insurableEntities(
-                                id number(10) primary key
-                              );
+CREATE TABLE IF NOT EXISTS billing."fundingSources"
+(
+  id_ numeric(10,0) primary key
+);
 
-create table fundingSources(
-                                id number(10) primary key
-                          );
+CREATE TABLE IF NOT EXISTS billing.jobs
+(
+  id_ numeric(10,0) primary key
+);
 
-create table jobs(
-                                id number(10) primary key
-                );
+CREATE TABLE IF NOT EXISTS billing.wages
+(
+  id_ numeric(10,0) primary key,
+	jobId numeric(10,0) references billing.jobs,
+	type_ varchar(50),
+	wage numeric(10,2)
+);
 
-create table providers(
-                                id number(10) primary key
-                      );
+CREATE TABLE IF NOT EXISTS billing.procurements
+(
+  id_ numeric(10,0) primary key,
+	type_ varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS billing.subscriptions
+(
+    id_ numeric(10,0) primary key,
+	type_ varchar(50),
+	cost_ numeric(10,2),
+	isRenewable numeric(1,0)
+);
+
+CREATE TABLE IF NOT EXISTS billing.penalties
+(
+  id_ numeric(10,0) primary key,
+	type_ varchar(50),
+	fee numeric(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS billing.insurance
+(
+  id_ numeric(10,0) primary key,
+	providerId numeric(10,0) references billing.providers,
+	plan varchar(50),
+	cost_ numeric(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS billing.funding
+(
+  id_ numeric(10,0) primary key,
+  type_ varchar(50)
+);
+
