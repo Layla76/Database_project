7d0555fb476b3c8a1403f673a252217538ba7340
Layla76 <47432002+Layla76@users.noreply.github.com>
2024-07-25 21:55:45 +0300

    Delete insert_files directory

--- insert_files/funding.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO funding (id_, type_) VALUES (100000000, 'book');
-INSERT INTO funding (id_, type_) VALUES (200000000, 'room');
-INSERT INTO funding (id_, type_) VALUES (300000000, 'building');
--- insert_files/penalties.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO penalties (id_, type_, fee) VALUES (111111111, 'late', 100.0);
-INSERT INTO penalties (id_, type_, fee) VALUES (222222222, 'lost', 300.0);
-INSERT INTO penalties (id_, type_, fee) VALUES (333333333, 'damaged', 1000.0);
