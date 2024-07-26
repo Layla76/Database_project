ad7ee2e745f664384f69c5e349a308247bc65ae7
Layla76 <47432002+Layla76@users.noreply.github.com>
2024-07-26 07:01:00 +0300

    Update ParamsQueries.sql

--- ParamsQueries.sql
+++ ParamsQueries.sql
@@ -1,26 +1,26 @@
 -- 1. Delete payments that were less than $[numeric]
 PREPARE del_payments (numeric) AS 
-DELETE FROM paid
+DELETE FROM payments
 USING employees, wages
-WHERE employees.id_ = paid.employee_id
-AND paid.wage_id = wages.id_
+WHERE employees.id_ = payments.employee_id
+AND payments.wage_id = wages.id_
 AND wages.wage < $1;
 
-EXECUTE del_payments(1000000000000);
+EXECUTE del_payments(90000);
 
 -- 2. How many items are insured for over $[numeric]?
 PREPARE count_insured (numeric) AS 
-SELECT COUNT(DISTINCT insurable_entities.id_)
-FROM insurable_entities
-JOIN insured ON insurable_entities.id_ = insured.insurable_entity_id
+SELECT COUNT(DISTINCT assets.id_)
+FROM assets
+JOIN insured ON assets.id_ = insured.asset_id
 JOIN insurance ON insured.insurance_id = insurance.id_
 WHERE insurance.cost_ > $1;
 
-EXECUTE count_insured(1000000);
+EXECUTE count_insured(5000);
 
 -- 3. The deadline for penalties is extended by [int] days
 PREPARE extend_penalty (int) AS 
-UPDATE receives
+UPDATE member_penalties
 SET due_date = due_date + $1;
 
 EXECUTE extend_penalty(1);
